67d01404b94f614e941d0a206ccb5c5914c7981c
yzx860325-design <yzx860325-design@users.noreply.github.com>
2026-08-05 15:55:26 +0800

    修复 dae/daed 互斥冲突: custom_feed 同步列表去掉老版 dae, 只保留 luci-app-daed(新版). daed 包由 immortalwrt/packages 官方 net/daed 提供. 根除 opkg Collide pkgs 错误

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -167,7 +167,7 @@ install_custom_feed() {
         luci-app-openclash luci-app-homeproxy luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
         msd_lite luci-app-msd_lite cups luci-app-cupsd \
-        dae luci-app-daed
+        luci-app-daed
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
